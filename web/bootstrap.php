56caa8da7c5413edc303e403904c25cf8d3af84e
Marlise <marlise.au@gmail.com>
2015-06-11 19:13:48 -0400

    Made page title and subheading dynamic

--- web/bootstrap.php
+++ web/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
     $website_settings = [
         'site_title' => 'MarliseU Industries',
+        'page_head' => 'My Portfolio',
+        'subheading' => 'Subheading',
+
     ];
 ?>
--- web/portfolio-4-col.php
+++ web/portfolio-4-col.php
@@ -126,8 +126,8 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Four Column Portfolio
-                    <small>Subheading</small>
+                <h1 class="page-header"><?= $website_settings['page_head'] ?>
+                    <small><?= $website_settings['subheading'] ?></small>
                 </h1>
                 <ol class="breadcrumb">
                     <li><a href="index.html">Home</a>
